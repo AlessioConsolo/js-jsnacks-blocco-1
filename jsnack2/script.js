51b0377344c87256e6e8820fe85e25f03c1ce178
AlessioConsolo <alessioconsolo3@gmail.com>
2024-07-01 17:23:30 +0200

    [UPD - jsnack2 - JS] Added button function with 6 variables that gets numbers value from html

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -4,3 +4,14 @@
 const lista_numeri = [];
 
 const button = document.getElementById("button");
+
+button.addEventListener("click", function () {
+  let numero1 = document.getElementById("numero1").value;
+  let numero2 = document.getElementById("numero2").value;
+  let numero3 = document.getElementById("numero3").value;
+  let numero4 = document.getElementById("numero4").value;
+  let numero5 = document.getElementById("numero5").value;
+  let numero6 = document.getElementById("numero6").value;
+
+  console.log(numero1, numero2, numero3, numero4, numero5, numero6);
+});
