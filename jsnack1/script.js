69fe6e3861ef3399b3ea0cf1043b7fd65b88e45c
AlessioConsolo <alessioconsolo3@gmail.com>
2024-07-01 16:11:54 +0200

    [UPD - jsnack1 - JS] Added variable that contains guests names and a variable for the button

---
+++ jsnack1/script.js
@@ -0,0 +1,11 @@
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+const lista_invitati = [
+  "Francesco Ferdinandi",
+  "Giovanni Giubotti",
+  "Ignazio La Russa",
+  "Pippo Franco",
+  "Carlo Conti",
+];
+
+const button = document.getElementById("Gatsby-button");
